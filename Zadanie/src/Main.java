3be933395afd23f1cdc0137c01bd1530f9e00874
Anseal <dzieciol.filip.it@gmail.com>
2019-05-13 02:19:04 +0200

    Remake HashMap values' treatment
    Changed old logical operations on HashMap to responsive ones and added counting method.

--- Zadanie/src/Main.java
+++ Zadanie/src/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main (String[] args) {
 
         String[] employees = {
-                "office A", "John", "Doe",
-                "office B", "John", "Brown",
-                "office C", "Mary", "Jones",
-                "office B", "Adam", "Rust",
-                "office C", "Cindy", "Frost",
-                "office A", "Kate", "Coe",
-                "office A", "Bill", "Brown",
+                "Office A", "John", "Doe",
+                "Office B", "John", "Brown",
+                "Office C", "Mary", "Jones",
+                "Office B", "Adam", "Rust",
+                "Office C", "Cindy", "Frost",
+                "Office A", "Kate", "Coe",
+                "Office A", "Bill", "Brown",
                 "Office D", "Jon",  "Tare",
                 "Office E", "Anna", "Marks"
         };
@@ -25,18 +25,21 @@ public class Main {
             eMap.get(employees[i]).add(new Person(employees[i+1], employees[i+2]));
         }
         System.out.println(eMap);
+
+        biggestOffice(eMap);
     }
 
-    public static void biggestOffice (
-            List<Person> officeAEmployees,
-            List<Person> officeBEmployees,
-            List<Person> officeCEmployees) {
-        if (officeAEmployees.size() > officeBEmployees.size() && officeAEmployees.size() > officeCEmployees.size()) {
-            System.out.println("office A: " + officeAEmployees.size() + " users");
-        } else if (officeBEmployees.size() > officeCEmployees.size()) {
-            System.out.println("office B: " + officeBEmployees.size() + " users");
-        } else {
-            System.out.println("office C: " + officeCEmployees.size() + " users");
+    public static void biggestOffice (Map<String, List<Person>> eMap) {
+        int mostEmployees = 0;
+        String officeName = "";
+        for (String e : eMap.keySet()) {
+            if (eMap.get(e).size() > mostEmployees){
+                mostEmployees = eMap.get(e).size();
+                officeName = e;
+            }
         }
+
+        System.out.println(officeName + " has the biggest amount (" + mostEmployees + ") of employees");
     }
+
 }
